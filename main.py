ae5db3c2ee21124065100a9092fc016618bf37b4
sinoro <signourmelo@gmail.com>
2025-06-14 17:18:29 +0800

    added a screen in game using a while loop

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import pygame
 from constants import *
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        screen.fill('black')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
