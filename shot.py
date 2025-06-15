0da9b817d77200ed8f293ee97f4850643fa29c51
sinoro <signourmelo@gmail.com>
2025-06-15 16:13:48 +0800

    added shot class and shooting

---
+++ shot.py
@@ -0,0 +1,18 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Shot(CircleShape):
+    def __init__(self,x,y,radius,velocity):
+        super().__init__(x,y,radius)
+        self.velocity = velocity
+
+    def draw(self,screen):
+        pygame.draw.circle(surface=screen,
+                            color='white',
+                            center=self.position,
+                            radius=self.radius,
+                            width=2)
+
+    def update(self,dt):
+        self.position += self.velocity * dt
